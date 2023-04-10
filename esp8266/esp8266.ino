1deaff4a89470037017dabd1082b4b998ecae2a9
LENOVO <huy0000aa@gmail.com>
2023-04-11 00:53:26 +0700

    timer - mode 2

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -13,10 +13,9 @@ WiFiUDP ntpUDP;//doi tuong giao thuc UDP
 NTPClient timeClient(ntpUDP, "pool.ntp.org");//doi tuong may khach NTPClient
 
 String path = "/";//khai bao bien duong dan
-unsigned long t = 0;//khai bao bien tam thoi gian
-String dataSensor;
-String dataControlDevice;
-String dataMode;
+String chuoiNhanDuoc, timeNow, hours, minutes, dataSensor, dataControlDevice, dataMode;
+String timeOutDevice[4], results[4], pumpSpeed, arrResults;
+signed long int timeOut[4];//khai bao bien tam thoi gian
 
 void setup() {
   // Khoi tao cac cong noi tiep
@@ -33,25 +32,33 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);//khoi tao ket noi den firebase  
   timeClient.begin();//khoi tao doi tuong NTPclient de lay thoi gian
   timeClient.setTimeOffset(25200);//cai dat mui gio phu hop = 7 * 60 *60
+  timeClient.update();//cap nhap mui gio 
   Serial.println();
   Serial.print("Connected");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());//in ra ip address cua esp8266 trong mang wifi
   Firebase.setString(firebaseData, path + "/CHEDO", "1");//khoi tao ban dau che do 1
+  timeOut[0] = 2147483647;
+  timeOut[1] = 2147483647;
+  timeOut[2] = 2147483647;
+  timeOut[3] = 2147483647;
 }
 void loop() { // run over and over
-  String chuoiNhanDuoc = "";
+  Serial.println("=================================================================");
+  timeNow = timeClient.getFormattedTime();//lay thoi gian gio:phut:giay 
+  Serial.println(timeNow);//in ra thoi gian gio:phut:giay 
+  Firebase.setString(firebaseData, path + "/THOIGIANCAPNHAT", timeNow);
+  chuoiNhanDuoc = "";
   Serial.println("Du lieu nhan duoc tu STM32F103C8T6: ");
   while (Serial.available()){
     char kiTuNhanDuoc = (char)Serial.read();
     if(kiTuNhanDuoc == '!'){
       Serial.println(chuoiNhanDuoc);
-      chuoiNhanDuoc = "";
-      timeClient.update();//bat dau lay du lieu thoi gian    
-      String formattedTime = timeClient.getFormattedTime() + "!";//dinh dang lai thoi gian gio - phut - giay 
-      Serial.println(formattedTime + " is sended to STM32F103C8T6");//in ra ip address cua esp8266 trong mang wifi  
+      chuoiNhanDuoc = "";   
+      timeNow = timeClient.getFormattedTime() + "!";//dinh dang lai thoi gian gio - phut - giay 
+      Serial.println(timeNow + " is sended to STM32F103C8T6");//in ra ip address cua esp8266 trong mang wifi  
       delay(1000);
-      Serial1.println(formattedTime);
+      Serial1.println(timeNow);
     }
     else if(kiTuNhanDuoc == '\n'){
       Serial.println(chuoiNhanDuoc);
@@ -63,6 +70,12 @@ void loop() { // run over and over
         dataMode = firebaseData.stringData();   
       if(dataMode == "1" || dataMode == "3"){
         dataControlDevice = chuoiNhanDuoc.substring(10, 14);
+        if(dataControlDevice.substring(0, 1) != "0"){
+          Firebase.setString(firebaseData, path + "/pumpSpeed", dataControlDevice.substring(0, 1));
+          dataControlDevice = "1" + dataControlDevice.substring(1, 4);
+        }
+        else
+          Firebase.setString(firebaseData, path + "/pumpSpeed", "1");
         Firebase.setString(firebaseData, path + "/TINHIEUDONGCO", dataControlDevice);
         delay(1000);
         Serial.print("Du lieu gui cho STM32F103C8T6: ");
@@ -72,19 +85,65 @@ void loop() { // run over and over
       else{
         if(Firebase.getString(firebaseData, path + "/TINHIEUDONGCO"))
           dataControlDevice = firebaseData.stringData();
-        Serial.print("Du lieu gui cho STM32F103C8T6: ");
-        dataControlDevice = dataControlDevice + "2";
-        Serial.println(dataControlDevice);
-        Serial1.println(dataControlDevice);
+        if(Firebase.getString(firebaseData, path + "/pumpSpeed"))
+          pumpSpeed = firebaseData.stringData();
+        if(Firebase.getString(firebaseData, path + "/timeoutPump"))
+          timeOutDevice[0] = firebaseData.stringData();
+        if(Firebase.getString(firebaseData, path + "/timeoutFan"))
+          timeOutDevice[1] = firebaseData.stringData();
+        if(Firebase.getString(firebaseData, path + "/timeoutBulb"))
+          timeOutDevice[2] = firebaseData.stringData();
+        if(Firebase.getString(firebaseData, path + "/timeoutCanopy"))
+          timeOutDevice[3] = firebaseData.stringData();
+
+        results[0] = dataControlDevice.substring(0, 1) == "0" ? "0" : pumpSpeed;
+        results[1] = dataControlDevice.substring(1, 2) == "0" ? "0" : "1";
+        results[2] = dataControlDevice.substring(2, 3) == "0" ? "0" : "1";
+        results[3] = dataControlDevice.substring(3, 4) == "0" ? "0" : "1";
+
+        if(timeOutDevice[0] != "" && timeOutDevice[0] != "0"){
+          Firebase.setString(firebaseData, path + "/timeoutPump", "0");
+          timeOut[0] = timeOutDevice[0].toInt() * 1000 + (signed long int)(millis());
+        }
+        if(timeOutDevice[1] != "" && timeOutDevice[1] != "0"){
+          Firebase.setString(firebaseData, path + "/timeoutFan", "0");
+          timeOut[1] = timeOutDevice[1].toInt() * 1000 + (signed long int)(millis());
+        }
+        if(timeOutDevice[2] != "" && timeOutDevice[2] != "0"){
+          Firebase.setString(firebaseData, path + "/timeoutBulb", "0");
+          timeOut[2] = timeOutDevice[2].toInt() * 1000 + (signed long int)(millis());
+        }
+        if(timeOutDevice[3] != "" && timeOutDevice[3] != "0"){
+          Firebase.setString(firebaseData, path + "/timeoutCanopy", "0");
+          timeOut[3] = timeOutDevice[3].toInt() * 1000 + (signed long int)(millis());
+        }
+        Serial.println(String(millis()) + " " + String(timeOut[0]) + " " + String(timeOut[1]) + " " + String(timeOut[2]) + " " + String(timeOut[3]));        
+        if((signed long int)(millis()) - timeOut[0] >= 0){
+          results[0] = pumpSpeed;
+          timeOut[0] = 2147483647;
+        }
+        if((signed long int)(millis()) - timeOut[1] >= 0){
+          results[1] = "1";
+          timeOut[1] = 2147483647;
+        }
+        if((signed long int)(millis()) - timeOut[2] >= 0){
+          results[2] = "1";
+          timeOut[2] = 2147483647;
+        }
+        if((signed long int)(millis()) - timeOut[3] >= 0){
+          results[3] = "1";
+          timeOut[3] = 2147483647;
+        }
+        Serial.println("Tin hieu dong co gui cho STM32F103C8T6: ");
+        dataControlDevice = results[0] + results[1] + results[2] + results[3];
+        Firebase.setString(firebaseData, path + "/TINHIEUDONGCO", dataControlDevice);
+        Serial.println(dataControlDevice + "2");
+        Serial1.println(dataControlDevice + "2");
       }
       chuoiNhanDuoc = "";
     }
     else
       chuoiNhanDuoc += kiTuNhanDuoc;     
   }
-  String formattedTime = timeClient.getFormattedTime();//dinh dang lai thoi gian gio - phut - giay 
-  Serial.println(formattedTime);//in ra ip address cua esp8266 trong mang wifi 
-//  Serial1.println(formattedTime);//in ra ip address cua esp8266 trong mang wifi
-  Firebase.setString(firebaseData, path + "/THOIGIANCAPNHAT", formattedTime);
   delay(1000);
 }
